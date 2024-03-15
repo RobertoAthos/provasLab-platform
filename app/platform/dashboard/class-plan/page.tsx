ffdec9cc019067a2bd2d05c65bd0fe49812e924f
Roberto Athos <robertoathos22@gmail.com>
2024-03-15 20:50:59 -0300

    feat: clean project

--- app/platform/dashboard/class-plan/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ClassPlan() {
-  return (
-    <div>page</div>
-  )
-}
--- components/FieldError.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { FieldError } from 'react-hook-form'
-
-export default function FieldErrorMessage({error}:{error: FieldError | undefined}) {
-  return (
-    <span className='text-red-500 font-semibold text-xs mt-2'>{error?.message}</span>
-  )
-}
